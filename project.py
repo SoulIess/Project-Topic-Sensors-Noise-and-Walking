e2f1e6cc9b42f20523b6dfab82fa1e9f4427361f
sya120 <sunny_yang@sfu.ca>
2022-04-11 21:09:39 +0000

    Upload New File

---
+++ project.py
@@ -0,0 +1,42 @@
+import sys
+import numpy as np
+import pandas as pd
+from utils import *
+
+def main():
+    #day 1 data
+    paths = ['data/ankle.csv', 'data/arm.csv', 'data/pocket.csv', 'data/ankle_2.csv', 'data/arm_2.csv', 'data/pocket_2.csv']
+    dfs = []
+
+    for path in paths:
+        dfs.append((setUp(path, 0.5, 0.5), path))
+
+    for df, path in dfs:
+        name = path.replace('data/', '')
+        name = name.replace('.csv', '')
+        outputAccelWithButter(df, name)
+
+    ankle, _ = dfs[0]
+    outputSpeed(ankle)
+    
+    #day 2 data
+    paths = ['data/forward.csv', 'data/backward.csv', 'data/male.csv', 'data/female.csv']
+    dfs = []
+    for path in paths:
+        dfs.append((setUp(path, 0.5, 0.5), path))
+        
+    for df, path in dfs:
+        name = path.replace('data/', '')
+        name = name.replace('.csv', '')
+        outputAccelWithButter(df, name)
+
+    forward,_ = dfs[0]
+    backward,_ = dfs[1]
+    outputFB(forward, backward)
+
+    male,_ = dfs[2]
+    female,_ = dfs[3]
+    outputGender(male, forward, female)
+
+if __name__ == '__main__':
+    main()
